2e37f87aa543828e98237a69f3910092631186e4
Erol Sancaktutan <erolsancaktutan@gmail.com>
2021-11-04 17:23:03 +0300

    Scroll listener added for rv

--- app/src/main/java/com/es/tmdb/ui/activity/MainActivity.kt
+++ app/src/main/java/com/es/tmdb/ui/activity/MainActivity.kt
@@ -2,10 +2,13 @@ package com.es.tmdb.ui.activity
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.es.tmdb.abstracts.BaseActivity
+import com.es.tmdb.abstracts.PaginationListener
 import com.es.tmdb.databinding.ActivityMainBinding
 import com.es.tmdb.ui.adapter.MoviesAdapter
 import com.es.tmdb.ui.fragment.DetailFragment
@@ -16,14 +19,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BaseActivity() {
     private var binding: ActivityMainBinding? = null
     var pageCount = 0
+    var isLoading = false
+    var isLastPage = false
     private val moviesViewModel: MoviesViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+        setScrollListener()
         createMovieListAdapter()
         setRVDivider(binding!!.moviesRV)
         observeMovieList()
+        binding!!.loadingTV.visibility = View.VISIBLE
         moviesViewModel.getTrendMovies(pageCount + 1)
     }
 
@@ -40,6 +47,29 @@ class MainActivity : BaseActivity() {
     private fun observeMovieList() {
         moviesViewModel.movieList().observe(this, {
            binding!!.moviesRV.adapter!!.notifyDataSetChanged()
+            binding!!.loadingTV.visibility = View.GONE
+            isLoading = false
+            isLastPage = false
+        })
+    }
+
+    private fun setScrollListener(){
+        binding!!.moviesRV.addOnScrollListener(object : PaginationListener(binding!!.moviesRV.layoutManager as LinearLayoutManager){
+            override fun loadMoreItems() {
+                isLoading = true
+                isLastPage = true
+                pageCount += 1
+                moviesViewModel.getTrendMovies(pageCount)
+                binding!!.loadingTV.visibility = View.VISIBLE
+            }
+
+            override fun isLastPage(): Boolean {
+                return isLastPage
+            }
+
+            override fun isLoading(): Boolean {
+                return isLoading
+            }
         })
     }
 
